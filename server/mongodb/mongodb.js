2f290c33ad1ffa542067ee0fc38264c0b63282ae
Basu Singh <basu.singh@hypd.in>
2025-03-02 23:49:27 +0530

    updated mongodb url formation

--- server/mongodb/mongodb.js
+++ server/mongodb/mongodb.js
@@ -1,12 +1,18 @@
 const Config = require("../config/config")
 const mongoose = require("mongoose")
+const dbConfig = Config.database
 
 async function setupMongoDBConnection(){
-    const url = Config.database.url
-    const connection = mongoose.connect(url)
+    const connection = mongoose.connect(getConnectionURL())
             .then(() => {console.log("Connected to MongoDB...")})
             .catch((err) => console.log("Error from MongoDB: ", err))  
     return connection
+}
+
+function getConnectionURL(){
+    const url = `${dbConfig.scheme}${dbConfig.username}:${dbConfig.password}@${dbConfig.host}/${dbConfig.dbname}`
+    console.log(url);
+    return url
 }
 
 module.exports = {
